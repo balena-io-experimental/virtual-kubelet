38d44949af1df6fd999e4ab7cfa7dabe2b6904ec
Marios Balamatsias <mbalamatsias@gmail.com>
2019-01-11 11:52:46 +0200

    provider: add skeleton
    Signed-off-by: Marios Balamatsias <mbalamatsias@gmail.com>

--- providers/balena/web-balena/lib/main.js
+++ providers/balena/web-balena/lib/main.js
@@ -6,4 +6,24 @@ app.get('/', function (req, res) {
   res.send(handlers.defaultRoute())
 })
 
+app.post('/createPod', function (req, res) {
+  res.sendStatus(200)
+})
+
+app.delete('/deletePod', function (req, res) {
+  res.sendStatus(404)
+})
+
+app.get('/getContainerLogs', function (req, res) {
+  res.sendStatus(404)
+})
+
+app.get('/getPodStatus', function (req, res) {
+  res.sendStatus(404)
+})
+
+app.get('/getPods', function (req, res) {
+  res.sendStatus(404)
+})
+
 app.listen(3000, () => console.log('Running on http://localhost:3000'))
