11045babb2429819817826311dac229625b5559d
Marios Balamatsias <mbalamatsias@gmail.com>
2019-02-07 11:40:42 +0200

    main-test: Fix indentation
    Signed-off-by: Marios Balamatsias <mbalamatsias@gmail.com>

--- providers/balena/web-balena/test/main-test.js
+++ providers/balena/web-balena/test/main-test.js
@@ -2,73 +2,73 @@ const handlers = require('../lib/handlers');
 const assert = require('assert');
 
 describe('balena-kubelet', function() {
-	it('should give Balena Kubelet', () => {
-		assert(handlers.defaultRoute() === 'Balena Kubelet');
-	});
+  it('should give Balena Kubelet', () => {
+    assert(handlers.defaultRoute() === 'Balena Kubelet');
+  });
 
-	it('should get the running pods on the node', () => {
-		assert.deepStrictEqual(handlers.getPods(), [{}]);
-	});
+  it('should get the running pods on the node', () => {
+    assert.deepStrictEqual(handlers.getPods(), [{}]);
+  });
 
-	it('should create a new Pod (in our context deploy to balena)', () => {
-		assert(handlers.createPod() === true);
-	});
+  it('should create a new Pod (in our context deploy to balena)', () => {
+    assert(handlers.createPod() === true);
+  });
 });
 
 describe('Neccesary Virtual Kubelet functions', function() {
-	it('should get node\'s capacity', () => {
-		original = {"cpu": "3", "memory": "3Gi", "pods": "3"};
-		returned = handlers.getCapacity();
-		assert.deepStrictEqual(original, returned);
-	});
+  it('should get node\'s capacity', () => {
+    original = {"cpu": "3", "memory": "3Gi", "pods": "3"};
+    returned = handlers.getCapacity();
+    assert.deepStrictEqual(original, returned);
+  });
 
-	it('should get node\'s condtions', () => {
-		original = [
-			{
-				"Type": "Ready",
-				"Status": "True",
-				"LastHeartbeatTime": new Date(),
-				"LastTransitionTime": new Date(),
-				"Reason": "KubeletReady",
-				"Message": "Kubelet is ready."
-			},
-			{
-				"Type": "NotReady",
-				"Status": "False",
-				"LastHeartbeatTime": new Date(),
-				"LastTransitionTime": new Date(),
-				"Reason": "KubeletNotReady",
-				"Message": "Kubelet is not ready."
-			}
-		]
-		returned = handlers.getNodeConditions();
-		assert.deepStrictEqual(original, returned);
-	});
+  it('should get node\'s condtions', () => {
+    original = [
+      {
+        "Type": "Ready",
+        "Status": "True",
+        "LastHeartbeatTime": new Date(),
+        "LastTransitionTime": new Date(),
+        "Reason": "KubeletReady",
+        "Message": "Kubelet is ready."
+      },
+      {
+        "Type": "NotReady",
+        "Status": "False",
+        "LastHeartbeatTime": new Date(),
+        "LastTransitionTime": new Date(),
+        "Reason": "KubeletNotReady",
+        "Message": "Kubelet is not ready."
+      }
+    ]
+    returned = handlers.getNodeConditions();
+    assert.deepStrictEqual(original, returned);
+  });
 
-	it('should get node\'s addresses (fake one for now)', () => {
-		original = [{
-			"Type": "InternalIP",
-			"Address": "1.2.3.4"
-		}]
-		returned = handlers.getNodeAddresses();
-		assert.deepStrictEqual(original, returned);
-	});
+  it('should get node\'s addresses (fake one for now)', () => {
+    original = [{
+      "Type": "InternalIP",
+      "Address": "1.2.3.4"
+    }]
+    returned = handlers.getNodeAddresses();
+    assert.deepStrictEqual(original, returned);
+  });
 });
 
 describe('Balena Platform Communication', function() {
-	it('should validate the container\'s BALENA_API_KEY', async () => {
-		var fakeAPIkey = 'thisIsAmockApiKey';
-		var fakeSdk = {
-			loginState: false,
-			auth: {
-				async loginWithToken(authToken) {
-					loginState = true;
-				},
-				async isLoggedIn() {
-					return loginState;
-				}
-			}
-		}
-		assert(await handlers.validateBalenaAPIkey(fakeSdk, fakeAPIkey) === true);
-	});
+  it('should validate the container\'s BALENA_API_KEY', async () => {
+    var fakeAPIkey = 'thisIsAmockApiKey';
+    var fakeSdk = {
+      loginState: false,
+      auth: {
+        async loginWithToken(authToken) {
+          loginState = true;
+        },
+        async isLoggedIn() {
+          return loginState;
+        }
+      }
+    }
+    assert(await handlers.validateBalenaAPIkey(fakeSdk, fakeAPIkey) === true);
+  });
 });
