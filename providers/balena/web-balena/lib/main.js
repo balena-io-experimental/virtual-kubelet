5cc645980e3c601f4ccab33ed393631ebe5fa3ce
Marios Balamatsias <mbalamatsias@gmail.com>
2019-02-13 20:24:27 +0200

    web-balena: Add basic implementation for createPod and getPodStatus
    Signed-off-by: Marios Balamatsias <mbalamatsias@gmail.com>

--- providers/balena/web-balena/lib/main.js
+++ providers/balena/web-balena/lib/main.js
@@ -1,52 +1,85 @@
-const express = require('express')
-const app = express()
-const handlers = require('./handlers')
-const balenaSDK = require('balena-sdk')
-const balena = balenaSDK()
+const express = require('express');
+const app = express();
+const handlers = require('./handlers');
+const balenaSDK = require('balena-sdk');
+const balena = balenaSDK();
 
-app.get('/', function (req, res) {
-  res.send(handlers.defaultRoute())
-})
+app.get('/', (req, res) => {
+  res.send(handlers.defaultRoute());
+});
 
-app.post('/createPod', function (req, res) {
-  res.sendStatus(200)
-})
+app.use('/createPod', express.json());
+app.use('/updatePod', express.json());
+app.use('/deletePod', express.json());
 
-app.put('/updatePod', function (req, res) {
-  res.sendStatus(200)
-})
+var pods = [];
+app.post('/createPod', (req, res) => {
+  console.log('-------CreatePod--------');
+  console.log(JSON.stringify(req.body));
+  pods.push(req.body);
+  console.log('-------End CreatePod--------');
+});
 
-app.delete('/deletePod', function (req, res) {
-  res.sendStatus(200)
-})
+app.put('/updatePod', (req, res) => {
+  console.log('-------UpdatePod--------');
+  console.log(req.body);
+  console.log('-------End UpdatePod--------');
+  res.sendStatus(200);
+});
 
-app.get('/getPod', function (req, res) {
-  res.sendStatus(404)
-})
+app.delete('/deletePod', (req, res) => {
+  console.log('-------DeletePod--------');
+  console.log(req.body);
+  console.log('-------End DeletePod--------');
+  res.sendStatus(200);
+});
 
-app.get('/getContainerLogs', function (req, res) {
-  res.sendStatus(404)
-})
+app.get('/getPod', (req, res) => {
+  console.log('-------GetPod--------');
+  console.log(req.query);
+  console.log('-------End GetPod--------');
+  var wantedPod = pods.find(pod => pod.metadata.name === req.query.name);
+  if (wantedPod) {
+    res.json(wantedPod.status);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
-app.get('/getPodStatus', function (req, res) {
-  res.sendStatus(404)
-})
+app.get('/getContainerLogs', (req, res) => {
+  console.log('-------GetContainerLogs--------');
+  console.log(req.query);
+  console.log('-------End GetContainerLogs--------');
+  res.sendStatus(404);
+});
 
-app.get('/getPods', function (req, res) {
-  res.json(handlers.getPods())
-})
+app.get('/getPodStatus', (req, res) => {
+  console.log('-------GetPodStatus--------');
+  console.log(req.query);
+  console.log('-------End GetPodStatus--------');
+  var wantedPod = pods.find(pod => pod.metadata.name === req.query.name);
+  if (wantedPod) {
+    wantedPod.status.phase = 'Running';
+    res.json(wantedPod.status);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
-app.get('/capacity', function (req, res) {
-  res.json(handlers.getCapacity())
-})
+app.get('/getPods', (req, res) => {
+  res.json(handlers.getPods());
+});
 
-app.get('/nodeConditions', function (req, res) {
-  res.json(handlers.getNodeConditions())
-})
+app.get('/capacity', (req, res) => {
+  res.json(handlers.getCapacity());
+});
 
-app.get('/nodeAddresses', function (req, res) {
-  res.json(handlers.getNodeAddresses())
-})
+app.get('/nodeConditions', (req, res) => {
+  res.json(handlers.getNodeConditions());
+});
 
+app.get('/nodeAddresses', (req, res) => {
+  res.json(handlers.getNodeAddresses());
+});
 
-app.listen(3000, () => console.log('Running on http://localhost:3000'))
+app.listen(3000, () => console.log('Balena Kubelet handler running on http://localhost:3000'));
